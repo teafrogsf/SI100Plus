f2f7b3c379d00018f43a5b26b01c51025ba3d57d
zambar <zambar@163.com>
2024-09-03 18:33:59 +0800

    test: dbg

--- Exercises/maze-game/solve.py
+++ Exercises/maze-game/solve.py
@@ -2,8 +2,55 @@ import pygame, sys
 from maze.manager import GameManager
 from maze.statics import BlockType
 import threading, time
+import bdb, cmd, linecache
 
 manager = GameManager()
+
+class MyDebugger(bdb.Bdb):
+    def user_line(self, frame):
+        filename = frame.f_code.co_filename
+        lineno = frame.f_lineno
+        line = linecache.getline(filename, lineno).strip()
+        print(f'Paused at {filename}:{lineno} - {line}')
+        pygame.display.set_caption(f"{line}")
+        if("logic.py" not in filename):
+            self.set_step()
+        else:
+            self.interaction(frame)
+
+    def interaction(self, frame):
+        self.frame = frame
+        DebuggerCLI(self).cmdloop()
+
+class DebuggerCLI(cmd.Cmd):
+    prompt = '(maze-debugger) '
+
+    def __init__(self, debugger: MyDebugger):
+        super().__init__()
+        self.debugger = debugger
+
+    def do_continue(self, arg):
+        'Continue execution'
+        self.debugger.set_continue()
+        return True  # Exits the cmdloop
+
+    def do_step(self, arg):
+        'Step to the next line (Setp into)'
+        self.debugger.set_step()
+        return True
+
+    def do_quit(self, arg):
+        'Quit the debugger'
+        self.debugger.set_quit()
+        return True
+
+    def do_next(self, arg):
+        'Step to the next line (Step over)'
+        self.debugger.set_next(self.debugger.frame)
+        return True
+
+    def default(self, line):
+        print(f'Unknown command: {line}')
 
 def turn_left():
     manager.turn_left()
@@ -33,11 +80,14 @@ def try_exit():
 def check_front():
     return manager.check_front()
 
+
 def _operation():
     from logic import operation
+    debugger = MyDebugger()
     while True:
-        operation()
+        debugger.run('operation()', globals=globals(), locals=locals())
         time.sleep(0.01)
+        # operation()
 
 def main():
     # Initial Draw
