8b416d2a496b3733d5c020dd505fb3600d9689a6
zambar <zambar@163.com>
2024-09-12 16:37:35 +0800

    feat: added some feature

--- assets/preproc.js
+++ assets/preproc.js
@@ -4,11 +4,12 @@ module.exports = (markdown, options) => {
   return new Promise((resolve, reject) => {
     return resolve(
       (options.makeTitle ? `
+<p style="font-size: 16px; color: #999; margin:5px; position: absolute;"><a href="..">Homepage</a> | <a href="?print-pdf">Printable Version</a></p>
 <div style="display: flex; justify-content: center; align-items: center; height: 700px;">
   <div style="text-align: center; padding: 40px; background-color: white; border: 2px solid rgb(0, 63, 163); border-radius: 20px; box-shadow: 0 0 20px rgba(0,0,0,0.1);">
     <h1 style="font-size: 48px; font-weight: bold; margin-bottom: 20px; color: #333;">${options.makeTitle.lecture || "请设置 makeTitle.lecture"}</h1>
     <p style="font-size: 24px; color: #666;">${options.makeTitle.title || "请设置 makeTitle.title"}</p>
-    <p style="font-size: 16px; color: #999; margin-top: 20px;">${options.makeTitle.detail || "请设置 makeTitle.detail"}</p>
+    <p style="font-size: 16px; color: #999; margin-top: 20px; margin-bottom:5px">${options.makeTitle.detail || "请设置 makeTitle.detail"}</p>
   </div>
 </div>\n\n<!--s-->\n\n` : "")
       +
