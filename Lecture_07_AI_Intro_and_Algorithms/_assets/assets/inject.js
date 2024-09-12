c0bddefd337d94ac1e2edc69603f7d2f13dc3374
HeZeBang <HeZeBang@users.noreply.github.com>
2024-09-12 08:54:57 +0000

    deploy: 8b416d2a496b3733d5c020dd505fb3600d9689a6

--- Lecture_07_AI_Intro_and_Algorithms/_assets/assets/inject.js
+++ Lecture_07_AI_Intro_and_Algorithms/_assets/assets/inject.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     console.log('Hello from inject.js');
-    // 获取所有的 <img> 元素
     const images = document.querySelectorAll('img');
-    console.log(images);
+    // console.log(images);
 
-    // 遍历所有的 <img> 元素并收集它们的 alt 属性值
     const altTexts = Array.from(images).map(img => img.getAttribute('alt') || '');
 
     const widths = altTexts.map(text => text.split('|').pop());
     console.log(widths);
 
-    // 将 widths 分别添加到每个元素的 width 中
-    Array.from(images).forEach((img, i) => img.setAttribute('width', widths[i] || ""));
+    Array.from(images)
+        .forEach((img, i) => img.setAttribute('width', !isNaN(Number(widths[i])) ? widths[i] : ""));
+
+    Array.from(document.querySelectorAll('a'))
+        .forEach(a => a.setAttribute('target', '_blank'));
 });
