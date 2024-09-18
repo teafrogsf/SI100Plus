2fe997dbe305112496fa67096c28f43eb0f93688
AstatineAi <AstatineAi@users.noreply.github.com>
2024-09-18 14:34:12 +0000

    deploy: 8c8956ee8a85a6f32e697bd809ba564cf517f685

---
+++ Lecture_x_Search/_assets/assets/inject.js
@@ -0,0 +1,23 @@
+options.plugins.push(RevealMenu)
+options.menu = {
+    loadIcons: false
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Hello from inject.js');
+    const images = document.querySelectorAll('img');
+    // console.log(images);
+
+    const altTexts = Array.from(images).map(img => img.getAttribute('alt') || '');
+
+    const widths = altTexts.map(text => text.split('|').pop());
+    console.log(widths);
+
+    Array.from(images)
+        .forEach((img, i) => {
+            if(Number(widths[i])) img.setAttribute('width', widths[i]);
+        });
+
+    Array.from(document.querySelectorAll('a'))
+        .forEach(a => a.setAttribute('target', '_blank'));
+});
